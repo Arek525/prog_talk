7f73a1c7e17fba763aee07278a3f83fa4c8ca22a
Arkadiusz Lorek <a.lorek.132@studms.ug.edu.pl>
2025-12-23 19:08:00 +0100

    added setting and validating password, through salt and hash

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
 
 const UserSchema = new mongoose.Schema(
   {
@@ -32,5 +33,21 @@ const UserSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+UserSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 64, 'sha512')
+    .toString('hex');
+};
+
+UserSchema.methods.validatePassword = function (password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 64, 'sha512')
+    .toString('hex');
+
+  return this.hash === hash;
+};
 
 module.exports = mongoose.model('User', UserSchema);
