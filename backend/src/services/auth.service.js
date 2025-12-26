dae2af34768dca9545cd36489386d7c4b8540fa3
Arkadiusz Lorek <a.lorek.132@studms.ug.edu.pl>
2025-12-27 00:17:24 +0100

    auth services

---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,46 @@
+const User = require('../models/User.model');
+const jwt = require('jsonwebtoken');
+
+async function register({email, password, country}){
+    const exists = await User.findOne({email});
+    if(exists){
+        throw new Error('Email already used');
+    }
+
+    const user = new User({
+        email,
+        country,
+        status: 'PENDING'
+    });
+
+    user.setPassword(password);
+    await user.save();
+
+    return user;
+}
+
+async function login({email, password}){
+    const user = await User.findOne({email});
+
+    if(!user) return null;
+
+    if(user.status !== 'ACTIVE'){
+        throw new Error('Accoutn not active');
+    }
+
+    const valid = user.validatePassword(password);
+    if(!valid) return null;
+
+    const token = jwt.sign(
+        {sub: user._id},
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.JWT_EXPIRES_IN}
+    )
+
+    return {user, token};
+}
+
+module.exports = {
+    register,
+    login
+};
