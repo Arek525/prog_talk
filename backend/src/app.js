65e22008e2a75e8e96f4fc6a470d97a933a1dd6b
Arkadiusz Lorek <a.lorek.132@studms.ug.edu.pl>
2025-12-27 03:27:55 +0100

    added topics to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const passport = require('./config/passport');
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());  
 app.use(passport.initialize());
 
-const authRoutes = require('./routes/auth.routes')
+const authRoutes = require('./routes/auth.routes');
+const topicRoutes = require('./routes/topic.routes');
 
 app.use('/auth', authRoutes);
+app.use('/topics', topicRoutes)
 
 module.exports = app;
