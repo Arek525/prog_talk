02b271fc92a6d34582ae683a23f7d3a7a3cbf221
Arkadiusz Lorek <a.lorek.132@studms.ug.edu.pl>
2025-12-27 03:29:06 +0100

    added topic controller

---
+++ backend/src/controllers/topic.controller.js
@@ -0,0 +1,64 @@
+const topicService = require('../services/topic.service');
+
+async function createRootTopic(req, res){
+    try{
+        const topic = await topicService.createRootTopic(
+            req.user._id,
+            req.body
+        );
+        res.status(201).json(topic);
+    } catch(err){
+        res.status(400).json({error: err.message});
+    }
+}
+
+async function createSubtopic(req, res){
+    try{
+        const topic = await topicService.createSubtopic(
+            req.user._id,
+            req.params.id,
+            req.body
+        );
+        res.status(201).json(topic);
+    } catch(err){
+        res.status(403).json({error: err.message});
+    }
+}
+
+async function updateTopic(req, res){
+    try{
+        const topic = await topicService.updateTopic(
+            req.user_id,
+            req.params.id,
+            req.body
+        );
+        res.json(topic);
+    } catch(err){
+        res.status(403).json({error: err.message});
+    }
+}
+
+async function getRootTopics(req, res){
+    const topics = await topicService.getRootTopics();
+    res.json(topics);
+}
+
+async function getTopic(req, res){
+    const topic = await topicService.getTopic(req.params.id);
+    if(!topic) return res.status(404).json({error: 'Not found'});
+    res.json(topic);
+}
+
+async function getTopicTree(req, res){
+    const tree = await topicService.getTopicTree(req.params.id);
+    res.json(tree)
+}
+
+module.exports = {
+  createRootTopic,
+  createSubtopic,
+  updateTopic,
+  getRootTopics,
+  getTopic,
+  getTopicTree,
+};
