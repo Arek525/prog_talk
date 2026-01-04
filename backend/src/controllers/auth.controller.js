45810dbba7367743887968991c978d95b2c8634d
Arkadiusz Lorek <a.lorek.132@studms.ug.edu.pl>
2026-01-05 00:22:56 +0100

    temporar change

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -32,7 +32,7 @@ async function login(req, res){
 
         res.cookie('access_token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
+            secure: true,
             sameSite: 'none',
             maxAge: 60 * 60 * 1000,
         });
