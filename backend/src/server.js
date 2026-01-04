dd312a8f02000ff82f98fe9c3da517d2ca2c698b
Arkadiusz Lorek <a.lorek.132@studms.ug.edu.pl>
2026-01-05 00:24:24 +0100

    added websosckets to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const http = require('http');
 const app = require('./app');
+const initSocket = require('./socket');
 
 mongoose.connect(process.env.MONGO_URI);
 
-app.listen(3000, () => {
+const server = http.createServer(app);
+initSocket(server);
+
+server.listen(3000, () => {
     console.log('ProgTalk backend running on port 3000')
 })
