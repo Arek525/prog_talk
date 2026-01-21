24b59adccdc4e9c6006fdc0a8dee453d811fda28
Arkadiusz Lorek <a.lorek.132@studms.ug.edu.pl>
2026-01-21 15:09:27 +0100

    index and app

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import { useAuthStore } from './stores/auth.store'
+  import AdminNotifications from './components/admin/AdminNotifications.vue'
+
+  const auth = useAuthStore();
+</script>
+
+<template>
+  <RouterView/>
+  <AdminNotifications v-if="auth.isAdmin"/>
+</template>
