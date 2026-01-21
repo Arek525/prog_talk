18c1d36dd0be719df76cf72a6ec6245b91bcf0cb
Arkadiusz Lorek <a.lorek.132@studms.ug.edu.pl>
2026-01-21 15:05:50 +0100

    banned view

---
+++ frontend/src/views/BannedView.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { useAuthStore } from '../stores/auth.store';
+    import { useRouter } from 'vue-router'
+
+    const auth = useAuthStore()
+    const router = useRouter()
+
+    async function logout() {
+        await auth.logout()
+        router.push('/login')
+    }
+</script>
+
+<template>
+    <div style="max-width: 600px; margin: 40px auto;">
+        <h1>Account banned</h1>
+        <p>Your account has been banned.</p>
+        <p><b>{{ auth.user?.email }}</b></p>
+        <button @click="logout">Logout</button>
+    </div>
+</template>
